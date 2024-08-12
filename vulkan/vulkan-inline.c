3da3e3a4f17529f60482b2b8aad92273b17aa828
Branden J Brown <zephyrtronium@gmail.com>
2024-08-11 23:40:59 -0500

    initial commit

---
+++ vulkan/vulkan-inline.c
@@ -0,0 +1,9 @@
+
+// Note kklib.h defines WIN32_LEAN_AND_MEAN. Convenient for vulkan.h as well.
+#include <kklib.h>
+#include <kklib/maybe.h>
+#include <vulkan.h>
+
+static kk_box_t kk_vulkan_create_instance(kk_context_t *ctx)
+{
+}
